df3458540ace7df4f09785bdc8cb096666edb4ef
Thien Phuoc <thienphuoc@pop-os.localdomain>
2021-10-10 10:12:27 +0700

    First commit

---
+++ src/main/java/SGU/Tourio/TourioApplication.java
@@ -0,0 +1,13 @@
+package SGU.Tourio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SGU/Tourio/TourioApplicationTests.java
@@ -0,0 +1,13 @@
+package SGU.Tourio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
