e8bb9e353b3702324d0ebeb47e61b22e0bdbe286
Thien Phuoc <thienphuoc@pop-os.localdomain>
2021-10-11 11:49:13 +0700

    Second commit

--- src/main/java/SGU/Tourio/TourioApplication.java
+++ src/main/java/SGU/Tourio/TourioApplication.java
@@ -1,13 +1,27 @@
 package SGU.Tourio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.jdbc.core.JdbcTemplate;
 @SpringBootApplication
-public class TourioApplication {
+public class TourioApplication implements CommandLineRunner{
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TourioApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "INSERT INTO account (full_name, user_name, pwd,role_id) VALUES (?,?, ?, ?)";
+
+		int result = jdbcTemplate.update(sql, "tathienphuoc", "tathienphuoc@gmail.com", "tathienphuoc",1);
+		if (result > 0) {
+			System.out.println("A new row has been inserted.");
+		}
+
+	}
 }
